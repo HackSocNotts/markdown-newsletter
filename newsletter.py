2ecb272101be10f14894b0db05898e72bedd8c0a
James Frost <git@frost.cx>
2020-11-01 13:08:16 +0000

    Shebang and thoughts on licensing.

---
+++ newsletter.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""
+Some license. I'm thinking of making the program itself open
+source but the style file should be closed source to protect
+our branding somewhat.
+"""
+
